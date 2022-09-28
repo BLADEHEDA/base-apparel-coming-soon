756e3e058c89a11a1f3deecf3dfeafd10c9c31ad
BLADEHEDA <93664026+BLADEHEDA@users.noreply.github.com>
2022-09-29 00:16:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// lets write the validation of the email inot 
+let email = document.querySelector("#email");
+let button= document.querySelector("#icon-arrow");
+
+// let me get the error div thatwill display the error 
